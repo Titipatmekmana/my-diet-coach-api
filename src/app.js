497b525377a4317e6ef5afb11dbe8dd5630c0dea
Titipat_Mekmana <titipatmekmana@gmail.com>
2023-02-02 00:04:52 +0700

    config middleware erro not-found port9999 done

--- src/app.js
+++ src/app.js
@@ -1,2 +1,34 @@
-// const { sequelize } = require("./models");
-// sequelize.sync({ force: true });
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const chalk = require("chalk");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+
+const notFoundMiddleware = require("./middlewares/not-found");
+const errorMiddleware = require("./middlewares/error");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(
+  rateLimit({
+    windowMs: 1000 * 60 * 15,
+    max: 100,
+    message: { message: "too many requests,please try again later" },
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 9000;
+app.listen(port, () =>
+  console.log(
+    chalk.bgYellowBright.italic.bold(`server running on port: ${port}`)
+  )
+);
